719b6b756c17e4c5342d17584bfe8ace3c69d609
AsemIshtayah <isht.asem@gmail.com>
2022-02-16 16:12:12 +0200

    Created our entities and refactred using Project Lombok

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -1,16 +1,13 @@
 package com.vega.springit;
 
-import com.vega.springit.config.SpringitProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableConfigurationProperties(SpringitProperties.class)
 public class SpringitApplication {
 
     private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
