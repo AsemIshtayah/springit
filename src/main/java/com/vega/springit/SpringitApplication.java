c7b90e73740d7b33c5027e5afc69f163e0d3343b
AsemIshtayah <isht.asem@gmail.com>
2022-02-19 13:07:14 +0200

    Reverting to f268c00673aa2ba05888810c3a5fd8ca4a1e2c5a

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -2,10 +2,8 @@ package com.vega.springit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringitApplication {
@@ -16,15 +14,6 @@ public class SpringitApplication {
         SpringApplication.run(SpringitApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner runner(){
-        return args -> {
-            log.error("CommandLineRunner.run();");
-            log.warn("CommandLineRunner.run();");
-            log.info("CommandLineRunner.run();");
-            log.debug("CommandLineRunner.run();");
-            log.trace("CommandLineRunner.run();");
-        };
-    }
+
 
 }
