d98298b04197bccd9a80c8b4fccf5fee296207a7
AsemIshtayah <isht.asem@gmail.com>
2022-02-20 14:22:55 +0200

    nothing

--- src/main/java/com/vega/springit/repository/LinkRepository.java
+++ src/main/java/com/vega/springit/repository/LinkRepository.java
@@ -3,6 +3,7 @@ package com.vega.springit.repository;
 import com.vega.springit.domain.Link;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LinkRepository extends JpaRepository<Link,Long> {
+import java.util.List;
 
+public interface LinkRepository extends JpaRepository<Link,Long> {
 }
