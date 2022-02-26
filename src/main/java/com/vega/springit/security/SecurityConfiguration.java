fb3679b7377055c63c52feeb96fa6da155c87834
AsemIshtayah <isht.asem@gmail.com>
2022-02-26 15:09:29 +0200

    profile and register

--- src/main/java/com/vega/springit/security/SecurityConfiguration.java
+++ src/main/java/com/vega/springit/security/SecurityConfiguration.java
@@ -31,7 +31,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
            .and()
                 .formLogin()
                 .loginPage("/login")//.permitAll()
-                .usernameParameter("email");
+                .usernameParameter("email")
+                .and()
+           .logout()
+                .and()
+           .rememberMe();
+
 //           .and()
 //                .csrf().disable()
 //                .headers().frameOptions().disable();
