b92f9514d9e06251d89ff62ffa1c0fc462f2ccc3
AsemIshtayah <isht.asem@gmail.com>
2022-02-27 18:29:03 +0200

    service layer ready

--- src/main/java/com/vega/springit/controller/LinkController.java
+++ src/main/java/com/vega/springit/controller/LinkController.java
@@ -4,6 +4,8 @@ import com.vega.springit.domain.Comment;
 import com.vega.springit.domain.Link;
 import com.vega.springit.repository.CommentRepository;
 import com.vega.springit.repository.LinkRepository;
+import com.vega.springit.service.CommentService;
+import com.vega.springit.service.LinkService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.annotation.Secured;
@@ -22,23 +24,23 @@ public class LinkController {
 
     private static final Logger logger = LoggerFactory.getLogger(LinkController.class);
 
-    private LinkRepository linkRepository;
-    private CommentRepository commentRepository;
+    private LinkService linkService;
+    private CommentService commentService;
 
-    public LinkController(LinkRepository linkRepository, CommentRepository commentRepository) {
-        this.linkRepository = linkRepository;
-        this.commentRepository = commentRepository;
+    public LinkController(LinkService linkService, CommentService commentService) {
+        this.linkService = linkService;
+        this.commentService = commentService;
     }
 
     @GetMapping("/")
     public String list(Model model) {
-        model.addAttribute("links", linkRepository.findAll());
+        model.addAttribute("links", linkService.findAll());
         return "link/list";
     }
 
     @GetMapping("/link/{id}")
     public String read(@PathVariable Long id, Model model) {
-        Optional<Link> link = linkRepository.findById(id);
+        Optional<Link> link = linkService.findById(id);
         if(link.isPresent()) {
             Link currentLink = link.get();
             Comment comment = new Comment();
@@ -67,7 +69,7 @@ public class LinkController {
             return "link/submit";
         } else {
             // saving our link
-            linkRepository.save(link);
+            linkService.save(link);
             logger.info("New link was saved");
             redirectAttributes
                     .addAttribute("id", link.getId())
@@ -82,7 +84,7 @@ public class LinkController {
         if( bindingResult.hasErrors()){
             logger.info("There was a problem adding a new Comment");
         } else{
-            commentRepository.save(comment);
+            commentService.save(comment);
             logger.info("New comment was saved successfully. ");
         }
         return "redirect:/link/" + comment.getLink().getId();
---
+++ src/main/java/com/vega/springit/service/CommentService.java
@@ -0,0 +1,18 @@
+package com.vega.springit.service;
+
+import com.vega.springit.domain.Comment;
+import com.vega.springit.repository.CommentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+    CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public Comment save(Comment comment){
+        return commentRepository.save(comment);
+    }
+}
